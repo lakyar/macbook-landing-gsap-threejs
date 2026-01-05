543982dd8944c7e7b0f72f4a981f8e0beb973653
Lak Yar <lynn39408@gmail.com>
2026-01-05 20:48:58 +0630

    initial

---
+++ src/constants/index.js
@@ -0,0 +1,10 @@
+const navLinks = [
+    { label: "Store" },
+    { label: "Mac" },
+    { label: "iPhone" },
+    { label: "Watch" },
+    { label: "Vision" },
+    { label: "AirPods" },
+];
+
+export {navLinks}
