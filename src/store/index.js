22bf96591ee3fbbce72c9dbc2a012d358e2f42dc
Lak Yar <lynn39408@gmail.com>
2026-01-07 19:19:46 +0630

    features section added

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
-import {create} from 'zustand'
+import { create } from "zustand";
 
 const useMacbookStore = create((set) => ({
-    color: '#2e2c2e',
-    setColor: (color) => set ({color}),
+    color: "#2e2c2e",
+    setColor: (color) => set({ color }),
 
     scale: 0.8,
-    setScale: (scale) => set ({scale}),
+    setScale: (scale) => set({ scale }),
 
-}))
+    texture: "/videos/feature-1.mp4",
+    setTexture: (texture) => set({ texture }),
+
+    reset: () =>
+        set({
+            color: "#2e2c2e",
+            scale: 0.08,
+            texture: "/videos/feature-1.mp4",
+        }),
+}));
 
 export default useMacbookStore;
