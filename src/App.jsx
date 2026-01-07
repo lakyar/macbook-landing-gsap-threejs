49fddc6fc58624d0eb4eac30aa53d8054c1b47b9
Lak Yar <lynn39408@gmail.com>
2026-01-07 18:16:48 +0630

    footer added

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,28 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import ProductViewer from "./components/ProductViewer";
 import Showcase from "./components/Showcase";
+import Performance from "./components/Performance";
+import Features from "./components/Features";
+import Highlights from "./components/Highlights";
+import Footer from "./components/Footer";
+
+
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/all";
 
-gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollTrigger);
 
 const App = () => {
     return (
         <main>
-          <Navbar />
-          <Hero />
-          <ProductViewer />
-          <Showcase />
+            <Navbar />
+            <Hero />
+            <ProductViewer />
+            <Showcase />
+            <Performance />
+            <Features />
+            <Highlights />
+            <Footer />
         </main>
     );
 };
